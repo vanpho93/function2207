95cb9db47631aff70670c7eef2ad08cc500c7024
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-22 14:58:15 +0700

    simple array

---
+++ c.js
@@ -0,0 +1,17 @@
+// const arr = new Array();
+const arr = [1, 3, 5, 'abcd'];
+
+const arrSubjects = ['NodeJS', 'React', 'Native', 'PHP', 'Android', 'iOS'];
+
+// console.log(arrSubjects[1]);
+// arrSubjects[1] = 'Java';
+
+// arrSubjects.push('Unity');
+// arrSubjects.pop();
+// arrSubjects.shift();
+// arrSubjects.unshift('Unity');
+
+// arrSubjects.splice(2, 2);
+arrSubjects.splice(2, 0, 'Unity', 'MEAN');
+
+console.log(arrSubjects);

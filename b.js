8730a5af2c401af426c64a55463f94bb86d53ca6
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-22 14:47:31 +0700

    function

--- b.js
+++ b.js
@@ -1,7 +1,8 @@
-function veHinh(n, checkPoint) {
+function veHinh(n, checkPoint, getColumns) {
     for (let i = 1; i <= n; i++) {
         let str = '';
-        for (let j = 1; j <= n; j++) {
+        const soCot = getColumns ? getColumns(n) : n;
+        for (let j = 1; j <= soCot; j++) {
             const dieuKien = checkPoint(i, j, n);
             str += dieuKien ? '*' : ' ';
         }
@@ -9,7 +10,25 @@ function veHinh(n, checkPoint) {
     }
 }
 
-veHinh(6, (i, j, n) => j <= i);
-veHinh(6, (i, j, n) => j >= i);
-veHinh(6, (i, j, n) => j <= n + 1 - i);
-veHinh(6, (i, j, n) => j >= n + 1 - i);
+// veHinh(6, (i, j, n) => j <= i);
+// veHinh(6, (i, j, n) => j >= i);
+// veHinh(6, (i, j, n) => j <= n + 1 - i);
+// veHinh(6, (i, j, n) => j > n - i);
+// veHinh(6, (i, j, n) => Math.abs(j - n) < i, n => n * 2 - 1);
+
+/* 
+n = 7;
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+*/
+
+veHinh(9, (i, j, n) => {
+    const center = (n + 1) / 2;
+    if (i <= center) return Math.abs(j - center) < i;
+    return Math.abs(j - center) <= n - i;
+});
